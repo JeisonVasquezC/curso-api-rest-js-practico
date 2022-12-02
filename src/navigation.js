d33f35b8609aff26c8a9b04a82ba26dcea41b311
JeisonVasquezC <jeisonvasquezc@gmail.com>
2022-12-01 19:41:43 -0500

    otros archivos

---
+++ src/navigation.js
@@ -0,0 +1,113 @@
+const navigator = () => {
+    console.log({ location });
+    
+    location.hash.startsWith('#trends')    ? trendsPage()       :
+    location.hash.startsWith('#search=')   ? searchPage()       :
+    location.hash.startsWith('#movie=')    ? movieDetailPage()  :
+    location.hash.startsWith('#category=') ? categoriesPage()   :
+    homePage()
+};
+
+const trendsPage = () => {
+    console.log('TRENDS!!!');
+
+    headerSection.classList.remove('header-container--long');
+    headerSection.style.background = '';
+    arrowBtn.classList.remove('inactive');
+    arrowBtn.classList.remove('header-arrow--white');
+    headerTitle.classList.add('inactive');
+    headerCategoryTitle.classList.remove('inactive');
+    searchForm.classList.add('inactive');
+
+    trendingPreviewSection.classList.add('inactive');
+    categoriesPreviewSection.classList.add('inactive');
+    genericSection.classList.remove('inactive');
+    movieDetailSection.classList.add('inactive');
+};
+
+const searchPage = () => {
+    console.log('SEARCH!!!');
+
+    headerSection.classList.remove('header-container--long');
+    headerSection.style.background = '';
+    arrowBtn.classList.remove('inactive');
+    arrowBtn.classList.remove('header-arrow--white');
+    headerTitle.classList.add('inactive');
+    headerCategoryTitle.classList.remove('inactive');
+    searchForm.classList.remove('inactive');
+
+    trendingPreviewSection.classList.add('inactive');
+    categoriesPreviewSection.classList.add('inactive');
+    genericSection.classList.remove('inactive');
+    movieDetailSection.classList.add('inactive');
+};
+
+const movieDetailPage = () => {
+    console.log('MOVIE!!!');
+
+    headerSection.classList.add('header-container--long');
+    // headerSection.style.background = '';
+    arrowBtn.classList.remove('inactive');
+    arrowBtn.classList.add('header-arrow--white');
+    headerTitle.classList.add('inactive');
+    headerCategoryTitle.classList.add('inactive');
+    searchForm.classList.add('inactive');
+
+    trendingPreviewSection.classList.add('inactive');
+    categoriesPreviewSection.classList.add('inactive');
+    genericSection.classList.add('inactive');
+    movieDetailSection.classList.remove('inactive');
+};
+
+const categoriesPage = () => {
+    console.log('CATEGORY!!!');
+
+    headerSection.classList.remove('header-container--long');
+    headerSection.style.background = '';
+    arrowBtn.classList.remove('inactive');
+    arrowBtn.classList.remove('header-arrow--white');
+    headerTitle.classList.add('inactive');
+    headerCategoryTitle.classList.remove('inactive');
+    searchForm.classList.add('inactive');
+
+    trendingPreviewSection.classList.add('inactive');
+    categoriesPreviewSection.classList.add('inactive');
+    genericSection.classList.remove('inactive');
+    movieDetailSection.classList.add('inactive');
+};
+
+const homePage = () => {
+    console.log('HOME');
+
+    headerSection.classList.remove('header-container--long');
+    headerSection.style.background = '';
+    arrowBtn.classList.add('inactive');
+    arrowBtn.classList.remove('header-arrow--white');
+    headerCategoryTitle.classList.add('inactive');
+    headerTitle.classList.remove('inactive');
+    searchForm.classList.remove('inactive');
+
+    trendingPreviewSection.classList.remove('inactive');
+    categoriesPreviewSection.classList.remove('inactive');
+    genericSection.classList.add('inactive');
+    movieDetailSection.classList.add('inactive');
+
+    if(trendingMoviesPreviewList.children.length === 0){
+        console.log('Trending API');
+        getTrendingMoviesPreview();
+    };
+    if(categoriesPreviewList.children.length === 0){
+        console.log('Categories API');
+        getCategoriesPreviuew();
+    };
+};
+
+
+
+arrowBtn.addEventListener('click', () => location.hash = '#home n');
+searchFormBtn.addEventListener('click', () => location.hash = '#search=');
+trendingBtn.addEventListener('click', () => location.hash = '#trends=');
+
+window.addEventListener('DOMContentLoaded', navigator, false);
+window.addEventListener('hashchange', navigator, false);
+
---
+++ src/nodes.js
@@ -0,0 +1,26 @@
+const $ = (id) => document.querySelector(id);
+
+const headerSection = $('#header'); // 16
+const trendingPreviewSection = $('#trendingPreview'); // 27
+const categoriesPreviewSection = $('#categoriesPreview'); // 36
+const genericSection = $('#genericList'); // 54
+const movieDetailSection = $('#movieDetail'); // 80
+
+// List & COntainer
+const searchForm = $('#searchForm'); // 21
+const trendingMoviesPreviewList = $('.trendingPreview-movieList'); // 33
+const categoriesPreviewList = $('.categoriesPreview-list'); // 39
+const movieDetailCategoriesList = $('#movieDetail .categories-list'); // 80 - 87
+const relatedMoviesContainer = $('.relatedMovies-scrollContainer'); // 104
+
+// Element
+const headerTitle = $('.header-title'); // 18
+const arrowBtn = $('.header-arrow'); // 17
+const headerCategoryTitle = $('.header-title--categoryView'); // 19
+
+const searchFormInput = $('#searchForm input'); // 21
+const searchFormBtn = $('#searchBtn'); // 23
+
+const trendingBtn = $('.trendingPreview-btn'); // 81
+const movieDetailScore = $('.movieDetail-score'); // 82
+const movieDetailDescription = $('.movieDetail-description'); // 83
